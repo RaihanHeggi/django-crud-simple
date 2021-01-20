6f9e60f5d01f3085f71857da34dd7b6eaf612f23
RaihanHeggi <syaraihanheggi@gmail.com>
2021-01-20 16:12:17 +0700

    Django CRUD

---
+++ employee_project/employee_app/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Employee
+
+
+class EmployeeForm(forms.ModelForm):
+    class Meta:
+        model = Employee
+        fields = ("fullname", "emp", "mobile", "position")
+        labels = {
+            "fulname": "Full Name",
+            "emp": "Employee Code",
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(EmployeeForm, self).__init__(*args, **kwargs)
+        self.fields["position"].empty_label = "Select"
+        self.fields["emp"].required = False
+
